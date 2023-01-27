53c3ad2c0b64fa4c625fd2368f336c9d216a5852
Pablo Villedae <pablov.e12345@gmail.com>
2023-01-26 21:45:18 -0800

    display results from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { getAllStartships } from './services/api-sw-api';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [starships, setStarships] = useState([])
+
+  useEffect(()=> {
+    const fetchMonsterList = async () => {
+      const starshipData = await getAllStartships()
+      setStarships(starshipData.results)
+    }
+    fetchMonsterList()
+  }, [])
+  console.log(starships)
+
   return (
   <>
     <h1>Slay</h1>
+    {starships.map(starship => 
+            <div key={starship.index}>
+              {starship.name}
+            </div>  
+          )}
   </>
   );
 }
--- src/services/api-sw-api.js
+++ src/services/api-sw-api.js
@@ -2,4 +2,5 @@ const baseUrl = "https://swapi.dev/api/"
 
 export async function getAllStartships(){
   const res = await fetch(`${baseUrl}/starships`)
+  return res.json()
 }
